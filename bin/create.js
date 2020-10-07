bb91ec774cb2bb2b3dcbcf03d83e1be1ed164093
Viktor Chernodub <chernodub98@gmail.com>
2020-10-07 20:36:22 +0700

    update script

--- bin/create.js
+++ bin/create.js
@@ -1,35 +1,72 @@
 #!/usr/bin/env node
 'use strict';
 
-// Deps
+// Native deps
 const cp = require('child_process');
 const path = require('path');
 const fs = require('fs');
 
 // Args
-const [boilerplateUrl, name] = process.argv.slice(2);
+// const [boilerplateUrl, name] = process.argv.slice(2);
+const [boilerplateUrl, name, prefix] = [
+  'https://github.com/chernodub/ng-template',
+  'test',
+  'test',
+];
 
 // Base vars
-const rootDir = process.cwd();
-console.log('Root: ', rootDir);
+const projectDir = path.join(process.cwd(), name);
 const ANGULAR_CONFIG_NAME = 'angular.json';
 
+// Install additional deps
+cp.execSync(`cd ${__dirname} && npm i && cd ${process.cwd()}`);
+
+// Deps
+const replaceInFiles = require('replace-in-files');
+
 // Clone boilerplate
-if (fs.existsSync(path.join(rootDir, name))) {
+console.log(name);
+if (fs.existsSync(projectDir)) {
   console.error(`Folder ${name} already exists.`);
   process.exit(1);
 }
-cp.execSync(`git clone ${boilerplateUrl} ${name}`);
+console.log('Fetching boilerplate...');
+cp.execSync(`git clone ${boilerplateUrl} ${name} -q`);
 
 // Check whether the boilerplate is angular project by looking for `angular.json`.
-if (!fs.existsSync(path.join(rootDir, name, ANGULAR_CONFIG_NAME))) {
+if (!fs.existsSync(path.join(projectDir, ANGULAR_CONFIG_NAME))) {
   console.log('Boilerplate is not an Angular project.');
   // Clean up and exit
   cp.execSync(`rm -rf ${name}`);
   process.exit(1);
 }
 
-// TODO remove
-cp.execSync('npm i');
-const cowsay = require('cowsay');
-console.log(cowsay.say({ text: 'sup' }));
+console.log('Preparing linting tools...');
+const lintConfigPath = path.join(projectDir, 'tslint.json');
+const modifiedLintConfig = {
+  extends: '@saritasa/tslint-config-angular',
+  ...JSON.parse(fs.readFileSync(lintConfigPath)),
+};
+fs.writeFileSync(lintConfigPath, JSON.stringify(modifiedLintConfig));
+
+console.log('Preparing boilerplate for you...');
+// Replace app name
+replaceInFiles({
+  files: [`${name}/*`, `${name}/**/*`],
+  from: /APP_NAME/g,
+  to: name,
+  ignore: ['**/node_modules/**'],
+}).then(() =>
+  replaceInFiles({
+    files: [`${name}/*`, `${name}/**/*`],
+    from: /APP_PREFIX/g,
+    to: name,
+    ignore: ['**/node_modules/**'],
+  }),
+);
+
+// Replace app prefix
+
+console.log('Trying to get latest deps...');
+cp.execSync(`cd ${projectDir} && npm i @saritasa/tslint-config-angular`);
+cp.execSync(`cd ${projectDir} && npm i && npx ng update && npm update`);
